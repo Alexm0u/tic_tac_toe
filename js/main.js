a55f58f2728b4ffbde200a644f0fde733d418e7d
Àlex <alexm0u.13@gmail.com>
2023-02-11 15:30:25 +0100

    Por fin coge los nombres del storage y los muestra en tablero

--- js/main.js
+++ js/main.js
@@ -1,17 +1,62 @@
-//Capturo los div que mostrarán los nombres
 
-let player1 = document.getElementById("player1");
-let player2 = document.getElementById("player2");
 
-//Leo los datos del sessionStorage y los guardo en una variable
+// //Genero un objeto de JavaScript que va a tener una propiedad para cada player
+let players = {
+    player1 : "",
+    player2 : ""
+}
+// //Capturo los input por su className... recordad que los captura en una htmlcollection
 
-let datosSesion = JSON.parse(sessionStorage.getItem("playersInfo"));
+let inputs = Array.from(document.getElementsByClassName("namePlayer"));
 
-//Aplico esos datos a los dos divs
+// //Mapeare el array inputs para darle a todos los elementos el evento addeventlistener input
+// //para controlar cuando vayamos escribiendo en ellos
 
-nameP1.innerHTML = `${datosSesion.player1}`;
-nameP2.innerHTML = `${datosSesion.player2}`;
+inputs.map(
+    elemento => {
+        elemento.addEventListener("input", ()=>{
 
+//             //Según vamos escribiendo en el input, iremos actualizando el objeto players..
+//             //metiendo los valores en la propiedad correspondiente (player1 si escribo en el input de player1 por ejemplo)
+
+            for(let jugador in players){
+                if(elemento.name == jugador){
+                    //Ahora es cuando meto el valor en el objeto
+                    players[jugador] = elemento.value;
+                }
+            }
+        })
+    }
+)
+
+
+// //Creo la función cambiaPantalla que guardará en sessionStorage y después cambiará de pantalla
+
+const cambiaPantalla = () => {
+
+//     //Primero comprobamos que los nombres de los players NO esten vacios....
+
+    if( (players.player1 === '') || (players.player2 === '') ){
+
+        //Si uno de los 2 está vacio...ejecuto un return y así salgo de la función
+
+        return;
+    }
+
+//     //Si llego a este punto es porque los nombres si que tenían algún valor..
+
+    sessionStorage.setItem("playersInfo", JSON.stringify(players));
+
+//     //Después de haber guardado .... cambiamos de página
+
+console.log("playersInfo")
+    setTimeout(()=>{
+
+        window.open("../pages/tablero.html","_self");
+    },500);
+
+
+}
 
 let tablero = Array.from(document.getElementsByClassName("celda"));
 
