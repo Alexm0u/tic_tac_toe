b9a0d2324eb4d6656b1da8a57ce40a6c710c4ec4
Àlex <alexm0u.13@gmail.com>
2023-02-11 10:08:19 +0100

    restructuracion pagina player select, creados botones de input

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+//Capturo los div que mostrarán los nombres
+
+let player1 = document.getElementById("player1");
+let player2 = document.getElementById("player2");
+
+//Leo los datos del sessionStorage y los guardo en una variable
+
+let datosSesion = JSON.parse(sessionStorage.getItem("playersInfo"));
+
+//Aplico esos datos a los dos divs
+
+nameP1.innerHTML = `${datosSesion.player1}`;
+nameP2.innerHTML = `${datosSesion.player2}`;
+
 
 
 let tablero = Array.from(document.getElementsByClassName("celda"));
@@ -44,4 +58,7 @@ tablero.map(
         })
     }
 )
+
+
+
 
