e4e0342962b3c08761366dbecceb55c09feaa250
Àlex <alexm0u.13@gmail.com>
2023-02-14 17:59:08 +0100

    añadido tanque funcion ganadora, no funciona de momento pero deja pintar

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 
 // //Genero un objeto de JavaScript que va a tener una propiedad para cada player
 let players = {
-    player1 : "",
-    player2 : ""
+    player1: "",
+    player2: ""
 }
 // //Capturo los input por su className... recordad que los captura en una htmlcollection
 
@@ -14,13 +14,13 @@ let inputs = Array.from(document.getElementsByClassName("namePlayer"));
 
 inputs.map(
     elemento => {
-        elemento.addEventListener("input", ()=>{
+        elemento.addEventListener("input", () => {
 
-//             //Según vamos escribiendo en el input, iremos actualizando el objeto players..
-//             //metiendo los valores en la propiedad correspondiente (player1 si escribo en el input de player1 por ejemplo)
+            //             //Según vamos escribiendo en el input, iremos actualizando el objeto players..
+            //             //metiendo los valores en la propiedad correspondiente (player1 si escribo en el input de player1 por ejemplo)
 
-            for(let jugador in players){
-                if(elemento.name == jugador){
+            for (let jugador in players) {
+                if (elemento.name == jugador) {
                     //Ahora es cuando meto el valor en el objeto
                     players[jugador] = elemento.value;
                 }
@@ -34,26 +34,26 @@ inputs.map(
 
 const cambiaPantalla = () => {
 
-//     //Primero comprobamos que los nombres de los players NO esten vacios....
+    //     //Primero comprobamos que los nombres de los players NO esten vacios....
 
-    if( (players.player1 === '') || (players.player2 === '') ){
+    if ((players.player1 === '') || (players.player2 === '')) {
 
         //Si uno de los 2 está vacio...ejecuto un return y así salgo de la función
 
         return;
     }
 
-//     //Si llego a este punto es porque los nombres si que tenían algún valor..
+    //     //Si llego a este punto es porque los nombres si que tenían algún valor..
 
     sessionStorage.setItem("playersInfo", JSON.stringify(players));
 
-//     //Después de haber guardado .... cambiamos de página
+    //     //Después de haber guardado .... cambiamos de página
 
-console.log("playersInfo")
-    setTimeout(()=>{
+    console.log("playersInfo")
+    setTimeout(() => {
 
-        window.open("../pages/tablero.html","_self");
-    },500);
+        window.open("../pages/tablero.html", "_self");
+    }, 500);
 
 
 }
@@ -80,6 +80,33 @@ let combinacionGanadora = [
 
 const comprueboGanador = () => {
     console.log(miTablero);
+    
+    if ((combinacionGanadora[0].innerHTML == "X") && (combinacionGanadora[1].innerHTML == "X") && (combinacionGanadora[2].innerHTML == "X") ||
+    (combinacionGanadora[3].innerHTML == "X") && (combinacionGanadora[4].innerHTML == "X") && (combinacionGanadora[5].innerHTML == "X") ||
+    (combinacionGanadora[6].innerHTML == "X") && (combinacionGanadora[7].innerHTML == "X") && (combinacionGanadora[8].innerHTML == "X") ||
+    (combinacionGanadora[0].innerHTML == "X") && (combinacionGanadora[3].innerHTML == "X") && (combinacionGanadora[6].innerHTML == "X") ||
+    (combinacionGanadora[1].innerHTML == "X") && (combinacionGanadora[4].innerHTML == "X") && (combinacionGanadora[7].innerHTML == "X") ||
+    (combinacionGanadora[2].innerHTML == "X") && (combinacionGanadora[5].innerHTML == "X") && (combinacionGanadora[8].innerHTML == "X") ||
+    (combinacionGanadora[0].innerHTML == "X") && (combinacionGanadora[4].innerHTML == "X") && (combinacionGanadora[8].innerHTML == "X") ||
+    (combinacionGanadora[2].innerHTML == "X") && (combinacionGanadora[4].innerHTML == "X") && (combinacionGanadora[6].innerHTML == "X")) {
+    setTimeout(() => { location.href = "looser.html"; }, 300);
+    // sessionStorage.setItem("nameWinner", name1);
+
+} else if ((combinacionGanadora[0].innerHTML == "O") && (combinacionGanadora[1].innerHTML == "O") && (combinacionGanadora[2].innerHTML == "O") ||
+    (combinacionGanadora[3].innerHTML == "O") && (combinacionGanadora[4].innerHTML == "O") && (combinacionGanadora[5].innerHTML == "O") ||
+    (combinacionGanadora[6].innerHTML == "O") && (combinacionGanadora[7].innerHTML == "O") && (combinacionGanadora[8].innerHTML == "O") ||
+    (combinacionGanadora[0].innerHTML == "O") && (combinacionGanadora[3].innerHTML == "O") && (combinacionGanadora[6].innerHTML == "O") ||
+    (combinacionGanadora[1].innerHTML == "O") && (combinacionGanadora[4].innerHTML == "O") && (combinacionGanadora[7].innerHTML == "O") ||
+    (combinacionGanadora[2].innerHTML == "O") && (combinacionGanadora[5].innerHTML == "O") && (combinacionGanadora[8].innerHTML == "O") ||
+    (combinacionGanadora[0].innerHTML == "O") && (combinacionGanadora[4].innerHTML == "O") && (combinacionGanadora[8].innerHTML == "O") ||
+    (combinacionGanadora[2].innerHTML == "O") && (combinacionGanadora[4].innerHTML == "O") && (combinacionGanadora[6].innerHTML == "O")) {
+
+    setTimeout(() => { location.href = "looser.html"; }, 300);;
+    // sessionStorage.setItem("nameWinner", name2)
+
+}
+
+
 
 }
 
@@ -102,7 +129,4 @@ tablero.map(
         })
     }
 )
-
-
-
 
